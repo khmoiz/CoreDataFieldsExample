99f917570ee2e76ff7533a92f3dc39a9aecc536e
Zakim Javer <javerzakim08@gmail.com>
2019-10-18 14:54:24 -0400

    Initial Commit

---
+++ BillCalc10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BillCalc10
+//
+//  Created by Zakim Javer on 2019-10-18.
+//  Copyright © 2019 Zakim Javer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
